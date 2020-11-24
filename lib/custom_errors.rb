a8fbe55dc1a2fca41c0074b029235c13a44e9966
Wesley <wspencer3295@yahoo.com>
2020-11-24 16:44:51 -0700

    commit tree 00189745d371d7843dffc87265caa352b375eabe

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,15 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
   end
+end
+
+class PartnerError < StandardError
+end
 
 end
 
